16c1f5be4dbe4e4e56c6b31a919f3de0bb10a9bb
Kevin <354017082@qq.com>
2016-09-21 12:16:02 +0800

    完善框架，开始写第一个CriticFragment

--- app/src/main/java/com/musketeer/ten/adapters/CriticAdapter.java
+++ app/src/main/java/com/musketeer/ten/adapters/CriticAdapter.java
@@ -1,7 +1,46 @@
 package com.musketeer.ten.adapters;
 
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Kevin on 2016/9/21.
  */
-public class CriticAdapter {
+public class CriticAdapter extends PagerAdapter {
+
+    private List<View> data;
+
+    public CriticAdapter(List<View> data) {
+        if (data!=null) {
+            this.data = data;
+        }else {
+            this.data = new ArrayList<>();
+        }
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        container.addView(data.get(position));
+        return data.get(position);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        super.destroyItem(container, position, object);
+        container.removeView(data.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return data != null ? data.size() : 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
 }
