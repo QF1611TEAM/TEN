06e0f62ed8441b463b63dc1001bc16d235b84869
Andy Mung <fo4r_me@163.com>
2016-09-22 20:44:18 +0800

    登录

--- app/src/main/java/com/musketeer/ten/ui/fragments/MineFragment.java
+++ app/src/main/java/com/musketeer/ten/ui/fragments/MineFragment.java
@@ -1,24 +1,49 @@
 package com.musketeer.ten.ui.fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.musketeer.ten.R;
+import com.squareup.picasso.Picasso;
+
+import org.xutils.image.ImageOptions;
+import org.xutils.x;
+
+import java.util.HashMap;
+import java.util.Set;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.tencent.qq.QQ;
 
 /**
  * Created by Kevin on 2016/9/20.
  */
-public class MineFragment extends BaseFragment implements View.OnClickListener {
+public class MineFragment extends BaseFragment implements Handler.Callback,View.OnClickListener, PlatformActionListener {
     public static final String TAG = MineFragment.class.getSimpleName();
     private View mLogin;
+    @BindView(R.id.head_img)
+    ImageView mHeadImage;
+    @BindView(R.id.usr_name)
+    TextView mUsrName;
+    private Handler mHandler = new Handler(this);
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        layout = inflater.inflate(R.layout.minefragment,container,false);
+        layout = inflater.inflate(R.layout.minefragment, container, false);
+        ButterKnife.bind(this,layout);
         return layout;
     }
 
@@ -32,5 +57,53 @@ public class MineFragment extends BaseFragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         //点击登入
+
+        ShareSDK.initSDK(getActivity());
+        Platform platform = ShareSDK.getPlatform(getActivity(), QQ.NAME);
+        platform.setPlatformActionListener(this);
+
+        platform.authorize();
+        platform.showUser(null);
+    }
+
+    @Override
+    public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+        Log.e(TAG, "onComplete: ");
+
+//        Set<String> keys = hashMap.keySet();
+//
+//        for (String key : keys) {
+//            Object value = hashMap.get(key);
+//            Log.e(TAG, "onComplete: "+key +"---->"+ value);
+//        }
+        String data []={(String) hashMap.get("figureurl_qq_2"), (String) hashMap.get("nickname")};
+        Message obtain = Message.obtain();
+        obtain.what = 100;
+        obtain.obj = data;
+        mHandler.sendMessage(obtain);
+    }
+
+    @Override
+    public void onError(Platform platform, int i, Throwable throwable) {
+        Log.e(TAG, "onError: ");
+    }
+
+    @Override
+    public void onCancel(Platform platform, int i) {
+        Log.e(TAG, "onCancel: ");
+    }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        if (msg.what == 100) {
+            String data [] = (String[]) msg.obj;
+//            Picasso.with(getContext()).load(data[0]).rotate(25).into(mHeadImage);
+            ImageOptions options = new ImageOptions.Builder()
+                    .setRadius(45)
+                    .build();
+            x.image().bind(mHeadImage,data[0], options);
+            mUsrName.setText(data[1]);
+        }
+        return false;
     }
 }
