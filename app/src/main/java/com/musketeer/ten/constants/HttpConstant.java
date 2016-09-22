d944a0df5accb7183e60daac7c74785cdb792405
刘学斌 <Brand_LXB@163.com>
2016-09-22 14:33:49 +0800

    FragmentToFrangmet传值成功

--- app/src/main/java/com/musketeer/ten/constants/HttpConstant.java
+++ app/src/main/java/com/musketeer/ten/constants/HttpConstant.java
@@ -6,10 +6,12 @@ package com.musketeer.ten.constants;
  * 存储网址
  */
 public class HttpConstant {
-
-    public static final String CIRTIC_URL="http://api.shigeten.net/api/Critic/GetCriticList";
     public static final String DIAGRAM_URL="http://api.shigeten.net/api/Diagram/GetDiagramList";
+    public static final String CIRTIC_URL="http://api.shigeten.net/api/Critic/GetCriticList";
 
+    public static final String PICASSO_IMAGE_URL="https://raw.githubusercontent.com/square/picasso/master/website/static/sample.png";
+
+    public static final String NOVELLIST_URL="http://api.shigeten.net/api/Novel/GetNovelList";
 
 
 }
