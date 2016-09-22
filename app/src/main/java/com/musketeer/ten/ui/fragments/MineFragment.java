585824a38bc3f68d6d52eeee8215f28de646cb83
Andy Mung <fo4r_me@163.com>
2016-09-22 17:36:44 +0800

    我的页面的布局

--- app/src/main/java/com/musketeer/ten/ui/fragments/MineFragment.java
+++ app/src/main/java/com/musketeer/ten/ui/fragments/MineFragment.java
@@ -11,12 +11,26 @@ import com.musketeer.ten.R;
 /**
  * Created by Kevin on 2016/9/20.
  */
-public class MineFragment extends BaseFragment {
+public class MineFragment extends BaseFragment implements View.OnClickListener {
     public static final String TAG = MineFragment.class.getSimpleName();
+    private View mLogin;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         layout = inflater.inflate(R.layout.minefragment,container,false);
         return layout;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mLogin = layout.findViewById(R.id.login);
+        mLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //点击登入
     }
 }
